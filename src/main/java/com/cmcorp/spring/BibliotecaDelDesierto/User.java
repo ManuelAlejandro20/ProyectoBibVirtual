60cdd0bcdb39251b9102a26295247e4f801ff378
MauroGM037 <mauricio.godoy@alumnos.ucn.cl>
2021-05-15 18:15:49 -0400

    se añadió: ControladorUser.java User.java

---
+++ src/main/java/com/cmcorp/spring/BibliotecaDelDesierto/User.java
@@ -0,0 +1,97 @@
+package com.cmcorp.spring.BibliotecaDelDesierto;
+
+public class User {
+    private int id;
+    private String email;
+    private String nickname;
+    private String password;
+    private String nombre;
+    private String apellidoP;
+    private String apellidoM;
+    private String direccion;
+    private String rol;
+
+    public User(int id, String email, String nickname, String password, String nombre, String apellidoP, String apellidoM, String direccion, String rol) {
+        this.id = id;
+        this.email = email;
+        this.nickname = nickname;
+        this.password = password;
+        this.nombre = nombre;
+        this.apellidoP = apellidoP;
+        this.apellidoM = apellidoM;
+        this.direccion = direccion;
+        this.rol = rol;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoP() {
+        return apellidoP;
+    }
+
+    public void setApellidoP(String apellidoP) {
+        this.apellidoP = apellidoP;
+    }
+
+    public String getApellidoM() {
+        return apellidoM;
+    }
+
+    public void setApellidoM(String apellidoM) {
+        this.apellidoM = apellidoM;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+}
