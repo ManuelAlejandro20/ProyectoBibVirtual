75a5457ab046caa7be6c833620a3a167af60fc69
cfuentes <68087089+Christian-FB@users.noreply.github.com>
2021-06-03 12:30:07 -0400

    feat: idioma entity, controller, service and repository added. (#10)

---
+++ src/main/java/com/cmcorp/spring/BibliotecaDelDesierto/repository/RepositorioIdioma.java
@@ -0,0 +1,8 @@
+package com.cmcorp.spring.BibliotecaDelDesierto.repository;
+
+import com.cmcorp.spring.BibliotecaDelDesierto.model.Idioma;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RepositorioIdioma extends JpaRepository<Idioma, Integer> {
+
+}
