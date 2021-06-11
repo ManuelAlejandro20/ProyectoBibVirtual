e8fc1ffd6f0a93a0d59a8d50216fc6dab729a53c
Manuel Trigo Montalban <manuel.trigo@alumnos.ucn.cl>
2021-06-11 11:50:45 +0100

    feat: unit tests files added

--- src/test/java/com/cmcorp/spring/BibliotecaDelDesierto/BibliotecaDelDesiertoApplicationTests.java
+++ src/test/java/com/cmcorp/spring/BibliotecaDelDesierto/BibliotecaDelDesiertoApplicationTests.java
@@ -3,11 +3,39 @@ package com.cmcorp.spring.BibliotecaDelDesierto;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+
 @SpringBootTest
 class BibliotecaDelDesiertoApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	private MockMvc mockMvc;
+
+	@BeforeEach
+	public void setup() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
 	}
+	
+	
+	@Test
+	void contextLoads() throws Exception{
+		mockMvc.perform(get("/users").accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType("application/json;charset=UTF-8"))
+        .andExpect(content().json("[{'id':1,'email':'manu@manu123.cl','nickname':'manuelalejandro20','password':'manu123','nombre':'Manuel','paterno':'Trigo','materno':'Montalban','direccion':'Palestina 1315','rol':'Usuario'}]"));	
+	}	
+	
 
 }
