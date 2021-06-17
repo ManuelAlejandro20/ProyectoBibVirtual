bc39cacf25862044258c9cc5f6dd6ab026b792af
Carlos Cortes <ccortes.csl@gmail.com>
2021-06-17 18:03:36 -0400

    docs: Add documentation and License of the ServicioIdioma

--- src/main/java/com/cmcorp/spring/BibliotecaDelDesierto/service/ServicioIdioma.java
+++ src/main/java/com/cmcorp/spring/BibliotecaDelDesierto/service/ServicioIdioma.java
@@ -8,24 +8,45 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+/**
+ * Service of Idioma
+ */
 @Service
 @Transactional
 public class ServicioIdioma {
+
     @Autowired
     private RepositorioIdioma repositorioIdioma;
 
+    /**
+     * GET all languages
+     * @return List<Idioma>
+     */
     public List<Idioma> listaIdiomas(){
         return repositorioIdioma.findAll();
     }
 
+    /**
+     * INSERT a language
+     * @param idioma
+     */
     public void saveIdioma(Idioma idioma){
         repositorioIdioma.save(idioma);
     }
 
+    /**
+     * GET a language by id
+     * @param id
+     * @return Idioma
+     */
     public Idioma getIdiomaXId(Integer id){
         return repositorioIdioma.findById(id).get();
     }
 
+    /**
+     * DELETE a language by id
+     * @param id
+     */
     public void deleteIdioma(Integer id){
         repositorioIdioma.deleteById(id);
     }
