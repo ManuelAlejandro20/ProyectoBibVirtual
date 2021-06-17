8932524233a47afa3619b2a0e606fffaef66317e
Carlos Cortes <ccortes.csl@gmail.com>
2021-06-17 17:40:37 -0400

    docs: Add documentation and License of the RepositorioUser

--- src/main/java/com/cmcorp/spring/BibliotecaDelDesierto/repository/RepositorioUser.java
+++ src/main/java/com/cmcorp/spring/BibliotecaDelDesierto/repository/RepositorioUser.java
@@ -5,10 +5,29 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+/**
+ * Interface of the User for encapsulating storage
+ */
 public interface RepositorioUser extends JpaRepository<User, Integer> {
+
+    /**
+     * Returns an user by his email
+     * @param email
+     * @return User
+     */
     User findUserByEmail(@Param("email") String email);
 
+    /**
+     * Returns a boolean value if the nickname exists or not
+     * @param nickname
+     * @return boolean
+     */
     boolean existsByNickname(@Param("nickname") String nickname);
 
+    /**
+     * Returns a boolean value if the email exists or not
+     * @param email
+     * @return boolean
+     */
     boolean existsByEmail(@Param("email") String email);
 }
