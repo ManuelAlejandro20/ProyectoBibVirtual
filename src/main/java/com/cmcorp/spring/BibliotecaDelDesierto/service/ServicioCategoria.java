c50e6de0938603ae92a2714d383189f4ab12f1c1
Carlos Cortes <ccortes.csl@gmail.com>
2021-06-17 17:59:52 -0400

    docs: Add documentation and License of the ServicioCategoria

--- src/main/java/com/cmcorp/spring/BibliotecaDelDesierto/service/ServicioCategoria.java
+++ src/main/java/com/cmcorp/spring/BibliotecaDelDesierto/service/ServicioCategoria.java
@@ -1,3 +1,38 @@
+/*
+ * Copyright (c) 2021 CMCORP
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * An intermediate form of license used by the X Consortium for X11 used the following wording:[16]
+ *
+ * Copyright (C) 2021 X Consortium
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE X CONSORTIUM BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Except as contained in this notice, the name of the X Consortium shall not be used in advertising or otherwise to promote the sale, use or other dealings in this Software without prior written authorization from the X Consortium.
+ *
+ * X Window System is a trademark of X Consortium, Inc.
+ */
+
 package com.cmcorp.spring.BibliotecaDelDesierto.service;
 
 import com.cmcorp.spring.BibliotecaDelDesierto.model.Categoria;
@@ -9,28 +44,54 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+/**
+ * Service of Categoria
+ */
 @Service
 @Transactional
 public class ServicioCategoria {
+
     @Autowired
     private RepositorioCategoria repositorioCategoria;
 
+    /**
+     * GET all categories
+     * @return List of categories
+     */
     public List<Categoria> listaCategorias(){
         return repositorioCategoria.findAll();
     }
 
+    /**
+     * INSERt a Category
+     * @param categoria
+     */
     public void saveCategoria(Categoria categoria){
         repositorioCategoria.save(categoria);
     }
 
+    /**
+     * GET a Category by id
+     * @param id Category
+     * @return Category
+     */
     public Categoria getCategoriaXId(Integer id){
         return repositorioCategoria.findById(id).get();
     }
 
+    /**
+     * GET a Category by name
+     * @param nombre
+     * @return Categoria
+     */
     public Categoria getCategoriaXNombre(String nombre){
         return repositorioCategoria.findByNombre(nombre);
     }
 
+    /**
+     * DELETE a Category by id
+     * @param id
+     */
     public void deleteCategoria(Integer id){
         repositorioCategoria.deleteById(id);
     }
